8cc2173cb21795743c1a5ba7c023f7d4de7b7b6d
Jack Blue <git.jack1624@gmail.com>
2025-04-19 13:47:43 +0700

    nothing v2

---
+++ Cinema/Application/Interfaces/Repositories/IItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Entities;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IItemRepository : IGenericRepository<Item>
+    {
+        Task<IEnumerable<Item>> GetByCategoryAsync(string itemCategory);
+    }
+}
--- Cinema/Application/Interfaces/Repositories/IUserRepository.cs
+++ Cinema/Application/Interfaces/Repositories/IUserRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data.Entities;
 
 namespace Application.Interfaces.Repositories
 {
     public interface IUserRepository
     {
+        Task<User?> GetByEmailAsync(string? email);
+        Task<User?> GetByUsernameAsync(string username);
+        Task<IEnumerable<User>> GetByRoleAsync(string role);
+        Task<IEnumerable<User>> GetByNameAsync(string? name);
+        Task<IEnumerable<User>> GetByPhoneAsync(string? phone);
     }
 }
